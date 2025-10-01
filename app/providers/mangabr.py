da1a8d69510301175f22bdd0f96edf814992c146
rennyson10oa <rennysonlimadiasrennyson@gmail.com>
2025-10-01 17:24:23 -0300

    atualizando tudo

--- app/providers/mangabr.py
+++ app/providers/mangabr.py
@@ -1,6 +1,5 @@
-import asyncio
-import html
-from urllib.parse import quote
+import re, html, asyncio
+from urllib.parse import quote, urljoin
 from bs4 import BeautifulSoup
 
 from app.core.base_provedor import BaseProvedor
@@ -75,7 +74,62 @@ class MangaBR(BaseProvedor):
         raise NotImplementedError
     
     async def get_chapters(self, url: str) -> list:
-        raise NotImplementedError
+        if self.session is None:
+            await self.criar_sessao()
+            
+        chapters = []
+        
+        print(f"[*] Carregando capítulos pela URL: {url}")
+        
+        try:
+            response = await self.session.get(url, headers={
+                "User-Agent": "Mozilla/5.0",
+                "X-Requested-With": "XMLHttpRequest"
+            })
+        except Exception as e:
+            print(f"[!] Erro ao acessar {url}: {e}")
+            return chapters
+        
+        if response.status_code != 200 or not response.text.strip():
+            print(f"[!] Resposta vazia ou erro {response.status_code} em {url}")
+            return chapters
+        
+        soup = BeautifulSoup(response.text, "html.parser")
+        links = soup.select(".col-chapter a")
+        
+        print(f"[*] Links de capítulos encontrados: {len(links)}")
+        
+        for link in links:
+            href = self.url + link.get("href")
+
+            # pega só o <h5> dentro do <a>
+            h5 = link.find("h5")
+            if not h5:
+                continue
+            
+            text = h5.get_text(strip=True)  # agora só o título/num do capítulo
+            
+            if not href or not text:
+                continue
+            
+            numero, titulo = await self.parse_chapter(text)
+            
+            if numero is None:
+                print(f"[!] Capítulo sem número detectado: '{titulo}' — pulando...")
+                continue  # ou numero = 0 se quiser inserir mesmo assim
+
+            chapters.append({
+                "numero": numero,
+                "titulo": titulo,  # usa só o título parseado
+                "url": href if href.startswith("http") else urljoin(self.url, href)
+            })
+
+        # ordena capítulos por número
+        chapters = sorted(chapters, key=lambda x: x["numero"] if x["numero"] else 0)
+
+        print(f"[*] Total de capítulos extraídos: {len(chapters)}")
+        return chapters
+
 
     async def sincronizar_mangas(self):
         """Busca mangas novos e capítulos novos, e atualiza o banco."""
@@ -90,7 +144,7 @@ class MangaBR(BaseProvedor):
 
 async def test():
     mangabr = MangaBR()
-    resultado = await mangabr.get_all_mangas()
+    resultado = await mangabr.get_chapters('https://mangabr.org/manga/unordinary')
     print(resultado)
 
 if __name__ == "__main__":
